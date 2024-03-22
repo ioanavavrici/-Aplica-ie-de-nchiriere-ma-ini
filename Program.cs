71ae5ef6cde204e22545fb642df2ca4b260106a3
Ioana Veronica Vavrici <ioana.vavrici@student.usv.ro>
2024-03-22 15:17:19 +0200

    actualizare clase

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using InchiriereMasina;
+using LibrarieMasini;
 namespace Inchiriere_masini
 {
     internal class Program
