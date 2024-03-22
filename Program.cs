57f73e2e1162c82367d9ebe7215ee9a8883c5b6b
Ioana Veronica Vavrici <ioana.vavrici@student.usv.ro>
2024-03-22 14:47:54 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inchiriere_masini
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
